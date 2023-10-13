22bc36bd33f244ee8f0f644843cea21ec147c8f5
ErikMak <47Poligons@gmail.com>
2023-10-13 19:46:57 +0300

    Настройка GitHub Actions

--- computerUniverse/vue.config.js
+++ computerUniverse/vue.config.js
@@ -6,5 +6,6 @@ module.exports = defineConfig({
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
 		}
-  }
+  },
+  publicPath: 'cu-termproject',
 })
