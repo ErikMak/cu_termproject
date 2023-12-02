32612dac2f1a4cedd79e09ba6a01c5f6db40ce72
ErikMak <47Poligons@gmail.com>
2023-12-02 17:58:55 +0300

    Разработка API

--- computerUniverseBackend/app/Models/Part.php
+++ computerUniverseBackend/app/Models/Part.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use App\Http\Filters\Filterable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Part extends Model
 {
     use HasFactory;
+    use Filterable;
 
     protected $primaryKey = 'model_number';
     protected $table = 'parts';
